e1923a9ad01173b756d3dd4dde919c0a566860fa
Hariom <harim987316@gmail.com>
2021-09-12 00:34:02 +0530

    Code check-in

---
+++ src/js/index.js
@@ -0,0 +1,10 @@
+let _menuToggle = document.querySelector('.toggle');
+let _navigation = document.querySelector('.navigation');
+let _main = document.querySelector('.main');
+
+
+_menuToggle.onclick = () => {
+    _menuToggle.classList.toggle('active');
+    _navigation.classList.toggle('active');
+    _main.classList.toggle('active')
+}
